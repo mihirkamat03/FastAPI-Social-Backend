f3a6205638a33daa8d33c6b63017361554e81efa
Mihir Kamat <kamatmihir03@gmail.com>
2026-06-21 23:52:52 +0530

    Revised code

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import Session
 from .routers import post, user, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
 
-# models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 origins=["https://www.google.com"]
 
